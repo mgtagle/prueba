c4fbdfeffbb7e355eb2f96915fb8545c86481d89
Marco <marcgonz@yahoo.com>
2019-08-05 16:35:08 -0500

    Script 0

---
+++ Clase0/Clase_0.R
@@ -0,0 +1,30 @@
+# Marco A Gonzalez
+# 05/08/2019
+# Clase 0
+
+
+# Pasos básicos -----------------------------------------------------------
+
+2+2
+a <- 2
+a * a
+a + 5
+
+
+# Importar datos ----------------------------------------------------------
+
+diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
+              11.8, 14.3, 12.5)
+
+diametro
+
+# Mediadas de tendencia central
+
+mean(diametro)
+median(diametro)
+
+# Medidas de dispersión
+
+sd(diametro)
+var(diametro)
+
